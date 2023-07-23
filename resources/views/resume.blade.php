5747ce4d1ac1d8afd22f03e7dd9644ffaff42117
Mohamed Hamed <m.hamed@yodawy.com>
2023-07-23 17:33:24 +0300

    first commit

---
+++ resources/views/resume.blade.php
@@ -0,0 +1,67 @@
+<div class="container w-2/3 mx-auto p-4">
+    <div class="flex justify-between mb-4">
+        <div>
+            <h1 class="text-2xl mb-2">Mohamed Hamed</h1>
+        </div>
+        <div>
+            <a href="#about" class="mx-2">About</a>
+            <a href="#skills" class="mx-2">Skills</a>
+            <a href="#experience" class="mx-2">Experience</a>
+            <a href="#education" class="mx-2">Education</a>
+            <a href="#portfolio" class="mx-2">Portfolio</a>
+            <a href="#references" class="mx-2">References</a>
+            <a href="#contact" class="mx-2">Contact</a>
+        </div>
+    </div>
+    
+
+    <div id="about" class="mb-8">
+        <h2 class="text-2xl mb-2">About</h2>
+        <p>Name: John Doe</p>
+        <p>Email: johndoe@example.com</p>
+        <p>Phone: +1 123-456-7890</p>
+        <p>Address: 123 ABC Street, City, State, Country</p>
+    </div>
+
+    <div id="skills" class="mb-8">
+        <h2 class="text-2xl mb-2">Skills</h2>
+        <ul>
+            <li>Web Development (HTML, CSS, JavaScript)</li>
+            <li>PHP and Laravel Framework</li>
+            <li>Database Management (MySQL)</li>
+            <li>Version Control (Git)</li>
+        </ul>
+    </div>
+
+    <div id="experience" class="mb-8">
+        <h2 class="text-2xl mb-2">Experience</h2>
+        <p>Company XYZ - Software Engineer</p>
+        <p>Duration: 20XX - Present</p>
+        <p>Responsibilities:</p>
+        <ul>
+            <li>Developing and maintaining web applications.</li>
+            <li>Collaborating with cross-functional teams.</li>
+            <li>Implementing software solutions.</li>
+        </ul>
+    </div>
+
+    <div id="education" class="mb-8">
+        <h2 class="text-2xl mb-2">Education</h2>
+        <p>Your educational background...</p>
+    </div>
+
+    <div id="portfolio" class="mb-8">
+        <h2 class="text-2xl mb-2">Portfolio</h2>
+        <p>Showcase your portfolio projects...</p>
+    </div>
+
+    <div id="references" class="mb-8">
+        <h2 class="text-2xl mb-2">References</h2>
+        <p>Recommendations and references...</p>
+    </div>
+
+    <div id="contact" class="mb-8">
+        <h2 class="text-2xl mb-2">Contact</h2>
+        <p>Contact information and form...</p>
+    </div>
+</div>
